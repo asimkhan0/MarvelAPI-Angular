b534754df3b3e03ec54cacf8f3dfef84798c84c6
Marc <marc@marcspicer.ca>
2018-07-09 23:00:51 +0500

    console.logs removed

--- js/controllers/appCtrl.js
+++ js/controllers/appCtrl.js
@@ -1,7 +1,6 @@
 var app = angular.module('app');
 
 app.controller('appCtrl', ['$scope','globalsvc', function($scope,globalsvc) {
-    console.log('app controller');
     $scope.loading = false;
     $scope.availableTags = [];
 }]);
--- js/controllers/characterCtrl.js
+++ js/controllers/characterCtrl.js
@@ -3,6 +3,5 @@ app.controller('characterCtrl', ['$scope', '$stateParams',
     function ($scope, $stateParams) {
         
         $scope.characterDetails = $stateParams.obj;
-        console.log($scope.characterDetails);
         $scope.characterDetails.picUrl = $scope.characterDetails.thumbnail.path + '.' + $scope.characterDetails.thumbnail.extension;
     }]);
